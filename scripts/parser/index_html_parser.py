4f295cfc8f30a077a038dcb44a25e453775b5b9c
Rafael Porres Molina <rafa@sourced.tech>
2019-09-19 19:01:52 +0200

    added script to update index
    Signed-off-by: Rafael Porres Molina <rafa@sourced.tech>

---
+++ scripts/parser/index_html_parser.py
@@ -0,0 +1,26 @@
+import html
+import html.parser
+
+class IndexHTMLParser(html.parser.HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.index_lines = []
+        self.a_data = {}
+
+    def handle_starttag(self, tag, attrs):
+        if tag == "a":
+            for attr in attrs:
+                self.a_data[attr[0]] = html.escape(attr[1])
+
+    def handle_endtag(self, tag):
+        if self.a_data:
+            self.index_lines.append(self.a_data)
+            self.a_data = {}
+
+    def handle_data(self, data):
+        if self.a_data:
+            self.a_data['data'] = data
+
+    def get_index_data(self, doc):
+        self.feed(doc)
+        return self.index_lines
